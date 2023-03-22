dbedd53d6fd2f37d40456929161dbe673f38609e
MichaelShawUK <MichaelShawUK@gmail.com>
2023-03-22 15:26:20 +0000

    Setup file structure

---
+++ models/club.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ClubSchema = new Schema({
+  name: { type: String, required: true, maxLength: 50 },
+  stadium: { type: String, required: true, maxLength: 50 },
+  founded: { type: String, maxLength: 4 },
+});
+
+ClubSchema.virtual("url").get(function () {
+  return `/club/${this._id}`;
+});
+
+module.exports = mongoose.model("Club", ClubSchema);
---
+++ models/player.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlayerSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  position: {
+    type: String,
+    required: true,
+    enum: ["Goalkeeper", "Defender", "Midfielder", "Forward"],
+  },
+  club: { type: String, required: true, maxLength: 100 },
+  country: { type: String, required: true, maxLength: 50 },
+  rating: { type: Number, min: 1, max: 100, required: true },
+  image: { type: String },
+});
+
+PlayerSchema.virtual("url").get(function () {
+  return `/player/${this._id}`;
+});
+
+module.exports = mongoose.model("Player", PlayerSchema);
