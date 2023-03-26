d5f39315681ae50e4af4478b216db8a8d85fd236
MichaelShawUK <MichaelShawUK@gmail.com>
2023-03-26 21:10:15 +0100

    Refactor club update function

--- controllers/clubController.js
+++ controllers/clubController.js
@@ -1,7 +1,5 @@
 const Club = require("../models/club");
 const Player = require("../models/player");
-const uploadImage = require("../utils/uploadImage");
-const deleteImage = require("../utils/deleteImage");
 const getImageUrl = require("../utils/getImageUrl");
 const { body, validationResult } = require("express-validator");
 
@@ -96,41 +94,37 @@ exports.club_update_post = [
   body("image", "Enter valid URL for club badge").trim().isURL(),
 
   async (req, res, next) => {
+    const updatedClub = {
+      name: req.body.name,
+      stadium: req.body.stadium,
+      founded: req.body.founded,
+      image: req.body.image,
+    };
+
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       res.render("club_form", {
         title: "Update Club",
-        club: {
-          name: req.body.name,
-          stadium: req.body.stadium,
-          founded: req.body.founded,
-          image: req.body.image,
-        },
+        club: updatedClub,
         errors: errors.array(),
       });
       return;
     }
 
     try {
-      const old_club = await Club.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          name: req.body.name,
-          stadium: req.body.stadium,
-          founded: req.body.founded,
-          image: req.body.image,
-        }
-      );
+      const old_club = await Club.findById(req.params.id);
+
+      let imageUrl = old_club.image;
 
       if (old_club.image !== req.body.image) {
-        await uploadImage(
-          { name: req.body.name, url: req.body.image },
-          Club,
-          "clubs"
-        );
+        imageUrl = await getImageUrl(req.body.image, "clubs");
       }
-      res.redirect("/club");
+
+      updatedClub.image = imageUrl;
+
+      await Club.updateOne({ _id: req.params.id }, updatedClub);
+      res.redirect(`/club/${req.params.id}`);
     } catch (err) {
       return next(err);
     }
